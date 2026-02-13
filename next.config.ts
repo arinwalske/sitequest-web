4d51f77f55c33c80a0dde275d9f55bcb33cf23f8
Andrew Rinwalske <arinwalske@gmail.com>
2026-02-12 20:19:47 -0500

    Didn't work again

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-return [
-    {
-      // This handles the Auth service specifically
-      source: '/auth/v1/:path*',
-      destination: 'https://bmbgppgkquphghhzorqh.supabase.co/auth/v1/:path*',
-    },
-    {
-      // This handles the Database/Rest service
-      source: '/rest/v1/:path*',
-      destination: 'https://bmbgppgkquphghhzorqh.supabase.co/rest/v1/:path*',
-    },
-  ];
-},
+    return [
+      {
+        source: '/auth/:path*',
+        destination: 'https://bmbgppgkquphghhzorqh.supabase.co/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
