4ada1d838d696176eff47bf7bc4e455dbd116ef2
Andrew Rinwalske <arinwalske@gmail.com>
2026-02-12 19:43:57 -0500

    Removing supabase gibberish from app login

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/supabase/:path*',
+        destination: 'https://bmbgppgkquphghhzorqh.supabase.co/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
